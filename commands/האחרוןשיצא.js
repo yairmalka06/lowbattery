44a9ad04fc136403c70ed841b893715ade4d9226
yairmalka06 <34348548+yairmalka06@users.noreply.github.com>
2018-05-18 00:52:05 +0300

    Add files via upload

---
+++ commands/האחרוןשיצא.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+
+module.exports.run = async (bot, message, args) => {
+
+message.channel.sendMessage("האחרון שיצא");
+
+}
+module.exports.help = {
+  name: "האחרוןשיצא"
+}
