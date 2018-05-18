2aa209b5c87979262757a055261c750496df1109
yairmalka06 <34348548+yairmalka06@users.noreply.github.com>
2018-05-18 12:11:12 +0300

    Add files via upload

---
+++ commands/cu.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+
+
+module.exports.run = async (bot, message, args) => {
+  var size;
+  bot.guilds.forEach(g =>{
+  size = g.memberCount
+  })
+var embed = new Discord.RichEmbed()
+.addField("מספר האנשים בשרת הוא "," ",size)
+  message.channel.send(embed);
+}
+module.exports.help = {
+  name: "cu"
+}
